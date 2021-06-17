3f41ca916e588dd59fa955a97dcde40429d46f38
Julian Toscani <julian_toscani@gmx.de>
2021-06-17 22:20:55 +0200

    add simple server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello World");
+import * as Koa from "koa";
+import * as Router from "koa-router";
+import * as logger from "koa-logger";
+import * as json from "koa-json";
+
+const PORT = 3000;
+
+const app = new Koa();
+const router = new Router();
+
+router.get("/", async (ctx: Koa.Context, next: Koa.Next) => {
+  ctx.body = { message: "Hello World" };
+  await next();
+});
+
+app.use(json());
+app.use(logger());
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
